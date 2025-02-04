c408dbb73d1938c3ccf03feaa84410bfbf62db6e
Klemen Lončar <loncarklemen@gmail.com>
2025-02-04 21:25:33 +0100

    Add Dashboard component feature

---
+++ client/src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import PortfolioSummary from '../../components/PortfolioSummary'
+import Header from '../../components/Header'
+import AssetSelector from '../../components/AssetSelector'
+import TradingInterface from '../../components/TradingInterface'
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="row flex-grow-1 m-0">
+      <Header />
+      <div className="card mb-4">
+        <div className="card-body">
+          <h1>
+            Welcome to <strong>Crypto Dashboard</strong>
+          </h1>
+        </div>
+      </div>
+
+      <div className="col-12">
+        <AssetSelector />
+      </div>
+      <div className="col-6">
+        <TradingInterface />
+      </div>
+      <div className="col-6">
+        <PortfolioSummary />
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
